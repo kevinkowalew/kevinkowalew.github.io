09c1c5dfd2b202a4fa9f1d17ea7d1dd1fa8c8d4f
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2026-08-04 11:26:04 -0500

    updates YOE

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ const App = () => {
 					<img className="avatar" alt="avatar" src="https://avatars.githubusercontent.com/u/9099021?v=4" />
 					<h1>Hi 👋 I'm Kevin</h1>
 					<h2>
-						A software engineer working remotely out of Chicago, IL.  I have over 6 years of industry experience building distributed systems at scale.  I have extensive experience with elastic systems and am constantly learning about emerging technologies in the Cloud Native space.  Outside of work, I enjoy spending time with my dog, playing guitar and exercising.
+						A software engineer working remotely out of Chicago, IL.  I have 8 years of industry experience building distributed systems at scale.  I have extensive experience with elastic systems and am constantly learning about emerging technologies in the Cloud Native space.  Outside of work, I enjoy spending time with my dog, playing guitar and exercising.
 					</h2>
 				</Typography>
 			</Box>
